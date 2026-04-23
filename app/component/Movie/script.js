80f301bdcf2dde8cbbf476c8fa8877216414d394
Halliez Baptiste <halliez1@mmi-dev.unilim.fr>
2026-04-23 13:56:39 +0200

    itération 1 clean, css à compléter

--- app/component/Movie/script.js
+++ app/component/Movie/script.js
@@ -1,29 +1,29 @@
-let templateFile = await fetch('./component/Movie/template.html');
+let templateFile = await fetch("./component/Movie/template.html");
 let template = await templateFile.text();
 
-let templateLiFile = await fetch('./component/Movie/templateLi.html');
+let templateLiFile = await fetch("./component/Movie/templateLi.html");
 let templateLi = await templateLiFile.text();
 
 let Movie = {};
 
-Movie.format = function(data, css=""){
-    let html = template;
-    html = html.replaceAll('{{cssClass}}', css);
-
-    let movieHTML = "";
-    for (let movie of data.movie){
-        let li = templateLi;
-        li = li.replaceAll('{{sourceAffiche}}', movie.image);
-        li = li.replaceAll('{{movieTitle}}', "../server/images" + movie.name);
-        movieHTML += li;
+Movie.format = function (data) {
+  let html = template;
+  if (data.length == 0) {
+    return html.replaceAll(
+      "{{movieList}}",
+      "<p class='movie__empty'>Aucun film n'est disponible pour le moment</p>",
+    );
+  } else {
+    let filmHTML = "";
+    for (let film of data) {
+      let li = templateLi;
+      li = li.replaceAll("{{sourceAffiche}}", "../server/images/" + film.image);
+      li = li.replaceAll("{{movieTitle}}", film.name);
+      filmHTML += li;
     }
-    html = html.replaceAll("{{movieList}}", movieHTML);
+    html = html.replaceAll("{{movieList}}", filmHTML);
     return html;
-}
-
-Movie.render = function(where, data, css=""){
-    let node = document.querySelector(where);
-    node.innerHTML += Checkout.format(data, css);
-}
+  }
+};
 
 export { Movie };
